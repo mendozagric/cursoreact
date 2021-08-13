af20720b5c130c11647ee91e853c4af2d9db9d60
Ricardo MendozaGonzález <mendozagric@aguascalientes.tecnm.mx>
2021-08-13 09:31:43 -0500

    mod

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,9 @@
 const Task = (props) =>{
 return(
-  <div className='task'>
-    <h3>{props.text}  <button className='delete'>X
-    </button></h3>
-    <p>{props.day}</p>
+  <div className={`task ${props.reminder ? 'reminder':''}`} onDoubleClick={() => props.onToggle(props.id)}>
+  <h3>{props.text} <button className='delete' onClick={() => props.onDelete(props.id)}>X
+  </button></h3>
+  <p>{props.day}</p>
   </div>
 )
 }
